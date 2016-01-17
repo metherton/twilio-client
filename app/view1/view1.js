16931dcf2ee7c1b52a16889b132310f81b332e8d
metherton <martinetherton63@gmail.com>
2016-01-17 21:17:09 +0100

    remove bootstrap

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+  angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap', 'ngResource'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -9,6 +9,26 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', 'tokenFactory', function($scope, tokenFactory) {
 
-}]);
+  $scope.token = tokenFactory.get().$promise.then(function(response) {
+    $scope.token = response;
+
+    window.worker = new Twilio.TaskRouter.Worker($scope.token.workerToken);
+
+    registerTaskRouterCallbacks();
+    bindAgentActivityButtons();
+
+  });
+
+}])
+.directive('aGreatEye', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<h1>lidless, wreathed in flame, {{1 + 1}} times</h1>'
+  };
+})
+    .factory('tokenFactory', [ '$resource', function($resource) {
+      return $resource('http://localhost:9001/tokens/:id', null);
+    }]);
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -6,10 +6,22 @@ describe('myApp.view1 module', function() {
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
+    var $httpBackend, scope;
+
+    it('should get a token', inject(function($controller, _$httpBackend_, $rootScope) {
+
+      $httpBackend = _$httpBackend_;
+
+      $httpBackend.expectGET("http://localhost:9001/tokens").respond({"workerToken":"123"});
+
+      scope = $rootScope.$new();
+
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
+      var view1Ctrl = $controller('View1Ctrl', {
+        $scope: scope
+      });
       expect(view1Ctrl).toBeDefined();
+      $httpBackend.flush();
     }));
 
   });
